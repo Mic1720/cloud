0d57db1a73582ce5afe7720b4179b124cbd86cb4
1601010 <1601010@034LAB3045>
2017-11-06 16:04:12 -0200

    Projeto backengine finalizado.

---
+++ MobileECloud(Final)/MobileCloudGames-4e2017_BATT/Backengine3/Web/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿namespace Web.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string NomePersonagem { get; set; }
+
+        public string Detalhes { get; set; }
+
+        public int Vida { get; set; }
+
+        public int Dano { get; set; }
+
+        public bool Hostil { get; set; }
+
+        // Relacionamento item -- TipoItem
+        public int TipoItemID { get; set; }
+
+        public virtual TipoItem _TipoItem { get; set; }
+
+    }
+}
